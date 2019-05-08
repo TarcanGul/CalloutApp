ffb791bd7b6d4c332835395ab4ec2b221f4053c5
nailt <nailtarcan@gmail.com>
2019-05-08 15:44:03 -0400

    Init dateTimeFilter

---
+++ CalloutApp Backend/dateTimeFilter.py
@@ -0,0 +1,13 @@
+import datetime
+
+def convertDateandTime(date_in_string, start_time_in_string, end_time_in_string):
+    #Should return a format
+    #Possible formats
+    '''
+    May 6, 2019
+    May 6 2019
+    05-06-2019
+    5-6-2019
+    May 6
+    05-06
+    '''
