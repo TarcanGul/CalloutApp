43ed509b0a5690df3d5300745876196082e4a88c
nailt <nailtarcan@gmail.com>
2019-05-18 17:45:58 -0400

    Added more dataExtractor tests. It seems like there are issues when pytesseract is spitting out typos.

--- CalloutApp Backend/tests/test_dataExtractor.py
+++ CalloutApp Backend/tests/test_dataExtractor.py
@@ -10,24 +10,105 @@ from PIL import Image
 TEST_IMAGES_FOLDER = os.getcwd() + "\\tests\\test_images"
 
 
-class test_dataExtractor(unittest.TestCase):
+class test_dataExtractor_date(unittest.TestCase):
+    def setUp(self):
+        self.logging_file = open("tests/ParsedText.log", "w")
+
     def test_1(self):
         wordsList = printList("test_1.jpg")
         infoExtractor = InfoExtractor()
+        self.logging_file.write(f"Wordlist from test_1 : \n{wordsList}\n")
         infoExtractor.extractWords(wordsList)
         self.assertEqual("2020-03-23", convertDate(infoExtractor.getDate()))
+    def test_2(self):
+        wordsList = printList("test_2.png")
+        infoExtractor = InfoExtractor()
+        self.logging_file.write(f"Wordlist from test_2 : \n{wordsList}\n")
+        infoExtractor.extractWords(wordsList)
+        self.assertEqual("2019-08-31", convertDate(infoExtractor.getDate()))
+    def test_3(self):
+        wordsList = printList("test_3.jpg")
+        infoExtractor = InfoExtractor()
+        self.logging_file.write(f"Wordlist from test_3 : \n{wordsList}\n")
+        infoExtractor.extractWords(wordsList)
+        self.assertEqual("2019-08-31", convertDate(infoExtractor.getDate()))
+    def tearDown(self):
+        self.logging_file.close()
+
+class test_dataExtractor_start_time(unittest.TestCase):
+    def setUp(self):
+        self.logging_file = open("tests/ParsedText.log", "w")
+
+    def test_1(self):
+        wordsList = printList("test_1.jpg")
+        infoExtractor = InfoExtractor()
+        self.logging_file.write(f"Wordlist from test_1 : \n{wordsList}\n")
+        infoExtractor.extractWords(wordsList)
         self.assertEqual("18:00:00", convertTime(infoExtractor.getTime()))
+    def test_2(self):
+        wordsList = printList("test_2.png")
+        infoExtractor = InfoExtractor()
+        self.logging_file.write(f"Wordlist from test_2 : \n{wordsList}\n")
+        infoExtractor.extractWords(wordsList)
+        self.assertEqual("19:00:00", convertTime(infoExtractor.getTime()))
+    def test_3(self):
+        wordsList = printList("test_3.jpg")
+        infoExtractor = InfoExtractor()
+        self.logging_file.write(f"Wordlist from test_3 : \n{wordsList}\n")
+        infoExtractor.extractWords(wordsList)
+        self.assertEqual("18:00:00", convertTime(infoExtractor.getTime()))
+    def tearDown(self):
+        self.logging_file.close()
+
+class test_dataExtractor_end_time(unittest.TestCase):
+    def setUp(self):
+        self.logging_file = open("tests/ParsedText.log", "w")
+
+    def test_1(self):
+        wordsList = printList("test_1.jpg")
+        infoExtractor = InfoExtractor()
+        self.logging_file.write(f"Wordlist from test_1 : \n{wordsList}\n")
+        infoExtractor.extractWords(wordsList)
         self.assertEqual("22:00:00", convertTime(infoExtractor.getEndTime()))
+    def test_2(self):
+        wordsList = printList("test_2.png")
+        infoExtractor = InfoExtractor()
+        self.logging_file.write(f"Wordlist from test_2 : \n{wordsList}\n")
+        infoExtractor.extractWords(wordsList)
+        self.assertEqual("22:00:00", convertTime(infoExtractor.getEndTime()))
+    def test_3(self):
+        wordsList = printList("test_3.jpg")
+        infoExtractor = InfoExtractor()
+        self.logging_file.write(f"Wordlist from test_3 : \n{wordsList}\n")
+        infoExtractor.extractWords(wordsList)
+        self.assertRaises(ValueError, infoExtractor.getEndTime)
+    def tearDown(self):
+        self.logging_file.close()
+
+class test_dataExtractor_location(unittest.TestCase):
+    def setUp(self):
+        self.logging_file = open("tests/ParsedText.log", "w")
+
+    def test_1(self):
+        wordsList = printList("test_1.jpg")
+        infoExtractor = InfoExtractor()
+        self.logging_file.write(f"Wordlist from test_1 : \n{wordsList}\n")
+        infoExtractor.extractWords(wordsList)
         self.assertEqual("Panda Express", infoExtractor.getLocations().pop())
     def test_2(self):
         wordsList = printList("test_2.png")
         infoExtractor = InfoExtractor()
+        self.logging_file.write(f"Wordlist from test_2 : \n{wordsList}\n")
         infoExtractor.extractWords(wordsList)
-        self.assertEqual("2019-08-31", convertDate(infoExtractor.getDate()))
-        self.assertEqual("19:00:00", convertTime(infoExtractor.getTime()))
-        self.assertEqual("22:00:00", convertTime(infoExtractor.getEndTime()))
         self.assertEqual("BRNG B247", infoExtractor.getLocations().pop())
-
+    def test_3(self):
+        wordsList = printList("test_3.jpg")
+        infoExtractor = InfoExtractor()
+        self.logging_file.write(f"Wordlist from test_3 : \n{wordsList}\n")
+        infoExtractor.extractWords(wordsList)
+        self.assertEqual("Matthew 210", infoExtractor.getLocations().pop())
+    def tearDown(self):
+        self.logging_file.close()
 
 
 
